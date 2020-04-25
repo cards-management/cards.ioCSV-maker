34cd7ab4ff6a6e76834783784b98305c8bc7b2a4
glaciermint <blank@blank.com>
2020-04-25 16:19:05 +0100

    Program now uses new lines for new cards Program now uses plain-text version of email, so there shouldn't be any formatting issues (also no need for BeautifulSoup any more) Program now accepts "prompt" or "answer" anywhere in the email subject Removed test parts of the program Program now closes and logs out of mail before ending Renamed some variables for better clarity

--- listifyer.py
+++ listifyer.py
@@ -1,114 +1,68 @@
 import imaplib, email
-from bs4 import BeautifulSoup
 import csv
-import re
-
-def striphtml(data):
-    p = re.compile(r'<.*?>')
-    return p.sub('', data)
 
 #logs into email and opens inbox
-user = ""
-password = ""
+email_user = ""
+email_pass = ""
 imap_url = "outlook.office365.com"
+
 mail = imaplib.IMAP4_SSL(imap_url)
-mail.login(user,password)
+mail.login(email_user,email_pass)
 mail.select("INBOX")
 
 #finds all emails
 result, data = mail.uid("search",None,"ALL")
 inbox_item_list = data[0].split()
 
-#stores processed email results
-test_array = [["label", "response"]]
-awnsers = [["label", "response"]]
+#variables to store processed email results
 prompts = [["label", "prompt"]]
+answers = [["label", "response"]]
 
 #analises each email
 for item in inbox_item_list:
-    print("==============next email=================")
+
     #extracts email content
     result2, email_data = mail.uid("fetch",item,"(RFC822)")
     raw_email = email_data[0][1].decode("utf-8")
     email_message = email.message_from_string(raw_email)
-    to_ = email_message["To"]
-    from_ = email_message["From"]
-    subject_ = email_message["Subject"]
-    for part in email_message.walk():
-        pass
-    content_type = part.get_content_type()
-    print(subject_)
-    print(content_type)
 
-    #determines email use
-    if subject_ == "Test":
-        html_ = part.get_payload()
-        soup = BeautifulSoup(html_, "html.parser")
-        text = soup.get_text()
-        #print(text)
-        testcards = text.split("#")
-        del testcards[-1]
-        #print(testcards)
-        for testees in testcards:
-            #print(testcards[testcards.index(testees)])
-            test_array +=[["", testcards[testcards.index(testees)]]]
-        #print(test_array)
-        print("valid")
-        
-    elif subject_ == "prompts" or subject_ == "prompt":
-        html_ = part.get_payload()
-        soup = BeautifulSoup(html_, "html.parser")
-        text = soup.get_text()
-        procards = text.split("#")
-        del procards[-1]
-        #print(cards)
-        for proms in procards:
-            prompts+=[["", procards[procards.index(proms)]]]
+    em_to = email_message["To"]
+    em_from = email_message["From"]
+    em_subject = email_message["Subject"]
 
-        print("added as:")
+    print("Reading email from " + em_from)
+    # Get plaintext version of email
+    em_text = ""
+    for payload in email_message.walk():
+        if payload.get_content_type().lower() == 'text/plain':
+            em_text = payload.get_payload()
+
+    if em_text == "":
+        print("Nothing found in email from " + em_from)
+
+
+    parts = em_text.split("\r\n")
+    parts = filter(None, parts) # Removes empty parts
+    
+    if "prompt" in em_subject.lower() or "question" in em_subject.lower():    
+        for proms in parts:
+            prompts+=[["", proms]]
         print(prompts)
-        print("valid")
 
-    elif subject_ == "answer" or subject_ == "answers" or subject_ == "awnsers":
-        html_ = part.get_payload()
-        soup = BeautifulSoup(html_, "html.parser")
-        text = soup.get_text()
-        anscards = text.split("#")
-        del anscards[-1]
-        #print(anscards)
-        for anses in anscards:
-            awnsers +=[["", anscards[anscards.index(anses)]]]
-        print("added as:")
-        print(awnsers)
-        print("valid")
-
-#tells results
-print("\n<<<<<<<<<<<<<<outputing as>>>>>>>>>>>>>>>")
-print("\n<<test>>")
-print(test_array)
-print("\n<<prompts>>")
-print(prompts)
-print("\n<<awnsers>>")
-print(awnsers)
+    elif "answer" in em_subject.lower():
+        for anses in parts:
+            answers +=[["", anses]]
+        print(answers)
 
 #exports as csvs
-with open("testcards.csv","w",newline='') as csvfile:
-    writer=csv.writer(csvfile)
-    [writer.writerow(r)for r in test_array]
 with open("promptcards.csv","w",newline='') as my_csv:
     csvWriter = csv.writer(my_csv,delimiter=',')
     csvWriter.writerows(prompts)
 with open("awnsercards.csv","w",newline='') as my_csv:
     csvWriter = csv.writer(my_csv,delimiter=',')
-    csvWriter.writerows(awnsers)
-
-#checks test csv accuracy
-testcards = list(csv.reader(open("testcards.csv")))
-print("\n<<<Test csv finally saved as:>>>")
-print(testcards)
+    csvWriter.writerows(answers)
 
 
-
-
-
-
+# Log out of mail
+mail.close()
+mail.logout()
